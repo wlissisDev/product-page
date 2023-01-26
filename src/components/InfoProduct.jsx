8706aab60f2f03825fb34946914b7ab9755024e9
wlissis <souzawlissis@gmail.com>
2023-01-25 22:31:51 -0300

    info

--- src/components/InfoProduct.jsx
+++ src/components/InfoProduct.jsx
@@ -3,6 +3,35 @@ import style from './infoproduct.module.css'
 
 export const InfoProduct = () => {
   return (
-    <div className={style.container}>InfoProduct</div>
+    <div className={style.container}>
+      <div className={style.contentInfo}>
+        <span className={style.subtitle}>SNEAKER COMPANY</span>
+        <h1 className={style.title}>Fall Limited Edition Sneakers</h1>
+        <p className={style.description}>
+          These low-profile sneakers are your perfect casual wear companion.
+          Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.
+        </p>
+
+        <div className={style['content-price']}>
+          <div className={style.price}>
+            <span>$125.00</span>
+            <span>50%</span>
+          </div>
+          <span className={style.descont}>$250.00</span>
+        </div>
+
+        <div className={style['add-cart']}>
+          <div className={style.amount}>
+            <span>-</span>
+            <span>0</span>
+            <span>+</span>
+          </div>
+          <button className={style['btn-add']}>
+            <img src="" alt="" />
+            Add to cart
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
--- src/components/ViewProduct.jsx
+++ src/components/ViewProduct.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 import style from './viewproduct.module.css';
+import thumbnail1 from '../images/image-product-1.jpg'
+import thumbnail2 from '../images/image-product-2.jpg'
+import thumbnail3 from '../images/image-product-3.jpg'
+import thumbnail4 from '../images/image-product-4.jpg'
 
 export const ViewProduct = () => {
   return (
-    <div className={style.container}>ViewProduct</div>
+    <div className={style.container}>
+      <div className={style['content-image']}>
+        <img className={style.image} src={thumbnail1} alt="imagem do produto" />
+      </div>
+      <div className={style['content-thumbnail']}>
+        <img className={style.thumbnail} src={thumbnail1} alt="imgem do produto" />
+        <img className={style.thumbnail} src={thumbnail2} alt="imgem do produto" />
+        <img className={style.thumbnail} src={thumbnail3} alt="imgem do produto" />
+        <img className={style.thumbnail} src={thumbnail4} alt="imgem do produto" />
+      </div>
+    </div>
+
   )
 }
